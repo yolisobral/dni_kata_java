ad911f821ad44cc42f6ca4dd2148f98d659f1252
T0T11 <tomas.orellano@gmail.com>
2026-03-27 01:48:45 +0100

    feat(Dni) subida de la clase dni

--- src/main/java/edu/teamrocket/Dni.java
+++ src/main/java/edu/teamrocket/Dni.java
@@ -1,25 +1,53 @@
 package edu.teamrocket;
 
+
 public class Dni {
 
     private final String dni;
-
-    private TablaAsignacion tabla = new TablaAsignacion();
+    private final TablaAsignacion tabla;
 
     public Dni(String dni) {
         this.dni = dni;
+        this.tabla = new TablaAsignacion();
     }
 
-    public String getDni() {
-        return this.dni;
+    public int extraerParteNumericaDni() {
+        return Integer.parseInt(dni.substring(0, 8));
     }
 
-    String extraerParteNumericaDni() {
-        return (String) dni.substring(0, dni.length() - 1);
+
+    public char extraerParteAlfabeticaDni() {
+        return dni.charAt(8);
+    }
+    public boolean isDniNumero(int numero) {
+        return numero >= 0 && numero <= 99999999;
     }
 
-    Boolean isDniNumero(String Cadena ){
+    public boolean checkNumeroDni() {
+        try {
+            int numero = extraerParteNumericaDni();
+            return isDniNumero(numero);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
-        return  true;
-    };
+
+    public char obtenerLetra() {
+        int numero = extraerParteNumericaDni();
+        int posicion = numero % tabla.getTabla().length;
+        return tabla.getLetra(posicion);
+    }
+
+
+    public boolean checkLetra() {
+        return extraerParteAlfabeticaDni() == obtenerLetra();
+    }
+
+
+    public boolean checkDni() {
+        return checkNumeroDni() && checkLetra();
+    }
 }
+
+
